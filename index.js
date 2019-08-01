cbaec5e89de7d4e4cc36eda48c21b4075a5251d0
Michael Vogel <michael.l.vogel@gmail.com>
2019-08-01 14:17:47 +0200

    Update code to ES2017

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-var express =  require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+
+const port = (process.env.PORT || 5000);
 
 const app = express();
 app.use(cors());
 
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
 
-app.get('/users', function(req, res) {
+app.get('/users', (req, res) => {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"},
-    {"id": 4,"firstName":"Mick","lastName":"Bord","email":"mbird@hotmail.com"}
+    { 'id': 1, 'firstName': 'Bob',   'lastName': 'Smith',  'email': 'bob@gmail.com' },
+    { 'id': 2, 'firstName': 'Tammy', 'lastName': 'Norton', 'email': 'tnorton@yahoo.com' },
+    { 'id': 3, 'firstName': 'Tina',  'lastName': 'Lee',    'email': 'lee.tina@hotmail.com' },
+    { 'id': 4, 'firstName': 'Mick',  'lastName': 'Bord',   'email': 'mbird@hotmail.com' }
   ]);
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port')); 
+app.listen(port, (err) => {
+  console.log(`Node app is running at localhost:${port}`); 
 });
